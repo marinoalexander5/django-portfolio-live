6b6411dff4982f956aa80ad4f561761dbcf06e5b
marinoalexander5 <marinoalexander5@gmail.com>
2021-04-16 18:35:59 -0300

    Updated requirements.txt. Added django-heroku.'

--- cvdigital/settings.py
+++ cvdigital/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,3 +167,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+django_heroku.settings(locals())
