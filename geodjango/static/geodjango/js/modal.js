1e485ee848235c0fe0aa340380ed0ef4eea8c626
marinoalexander5 <marinoalexander5@gmail.com>
2021-04-30 01:40:54 -0300

    Deleted local static to reduce heroku slug

--- geodjango/static/geodjango/js/modal.js
+++
@@ -1,3 +0,0 @@
-$(window).on('load', function(){
-    $('#exampleModal').modal('show');
-});
--- img_classifier/static/img_classifier/js/spinner.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( function() { 
-  $('#spinner').addClass('hide');
-  $('#upload-img').on('click', function()
-  {
-    $('#spinner').removeClass('hide');
-  });
-});
