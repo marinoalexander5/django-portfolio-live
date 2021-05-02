8a36b197dd909142cca33ac9df8372cfba743158
marinoalexander5 <marinoalexander5@gmail.com>
2021-05-01 23:28:00 -0300

    Fixed users app frontend

--- cvdigital/settings.py
+++ cvdigital/settings.py
@@ -182,10 +182,11 @@ LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 578
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 
 USE_S3 = False#os.environ.get('USE_S3') == 'True'
 
