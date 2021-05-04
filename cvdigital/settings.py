bb95a335822b554dd94de55a265de167533b0f8d
marinoalexander5 <marinoalexander5@gmail.com>
2021-05-04 12:52:25 -0300

    Erased iris classifier from navbar

--- cvdigital/settings.py
+++ cvdigital/settings.py
@@ -188,7 +188,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
-USE_S3 = os.environ.get('USE_S3') == 'True'
+USE_S3 = False#os.environ.get('USE_S3') == 'True'
 
 if USE_S3:
     # AWS storage
